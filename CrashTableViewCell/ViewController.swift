cf3225c8c4fab0b08bed7049ce31ec198f044290
Ben-G <Benjamin.Encz@gmail.com>
2015-07-09 11:43:44 -0700

    initial commit

---
+++ CrashTableViewCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrashTableViewCell
+//
+//  Created by Benjamin Encz on 7/9/15.
+//  Copyright © 2015 Benjamin Encz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
